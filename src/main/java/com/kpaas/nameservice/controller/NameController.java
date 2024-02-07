5d43fdb6c2e3e6f0808aa654da60f10a5cd9e29e
mctlmk <minkyu.lee@namutech.co.kr>
2024-02-07 17:09:01 +0900

    update

---
+++ src/main/java/com/kpaas/nameservice/controller/NameController.java
@@ -0,0 +1,13 @@
+package com.kpaas.nameservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NameController {
+
+    @GetMapping("/name")
+    public String getName() {
+        return "World";
+    }
+}
